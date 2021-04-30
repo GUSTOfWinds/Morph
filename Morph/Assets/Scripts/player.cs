471ee5d2be331524950650060df94391f513946a
Markus <markus.larsson421@gmail.com>
2021-04-30 16:03:42 +0200

    Readded models and scripts and my scene

--- Morph/Assets/Scripts/player.cs
+++ Morph/Assets/Scripts/player.cs
@@ -22,9 +22,9 @@ public class player : MonoBehaviour{
 	private void FixedUpdate() {
 		if(canMove) {
 			Vector3 velocity = new Vector3(joystick.Horizontal, 0.0f, joystick.Vertical);
-			//rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime * speedMultiplier);
-			gameObject.transform.rotation = Quaternion.LookRotation(velocity).normalized;
+			gameObject.transform.rotation = Quaternion.LookRotation(velocity).normalized; //Won't work using Rigidbody, use Rigidbody.AddTorque() instead
 			gameObject.transform.Translate(velocity * speedMultiplier * Time.deltaTime, Space.World);
+			//rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime * speedMultiplier); //Not working for some reason, jitters a lot
 		}
 	}
 
@@ -41,16 +41,15 @@ public class player : MonoBehaviour{
 
 	private void SelectedObject() {
 		//For mouse
-		Debug.Log("Mouse position: " + Input.mousePosition + ". Mouse 1 down: " + Input.GetMouseButtonDown(0));
 		if(Input.GetMouseButtonDown(0)) {
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 30f);
 			RaycastHit hit;
 			if(Physics.Raycast(ray, out hit)) {
-				Debug.Log(hit.transform.name);
-				if(hit.collider != null) {
-					string selectedObject = hit.transform.gameObject.name;
-					Debug.Log("Selected: " + selectedObject + ".");
+				//Debug.Log(hit.transform.name);
+				if(hit.collider != null && hit.transform.gameObject.tag.Equals("MorphableObject")) {
+					GameObject selectedObject = hit.transform.gameObject;
+					//Debug.Log("Selected: " + selectedObject + ".");
 					gameObject.GetComponent<morph>().morphObject(selectedObject);
 				}
 			}
