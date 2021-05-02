21ad2efb86511b7cb55015721de952bc10c8678a
Markus <markus.larsson421@gmail.com>
2021-05-02 21:57:27 +0200

    Fixed player and a bunch of other things!

--- Morph/Assets/Scripts/player.cs
+++ Morph/Assets/Scripts/player.cs
@@ -3,22 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
-	public float speedMultiplier = 5.0f;
+	public int movementSpeed = 10;
+	public float turnTime = .1f;
 	public Joystick joystick;
 
 	private Rigidbody rb;
 	private GameObject startObject;
+	private float turnVelocity;
 	private bool canMove;
 	TouchPhase touchPhase; //Don't remove!
-
-	public bool keyboardAndMouse;
 
 	private void Start() {
 		canMove = true;
 		touchPhase = TouchPhase.Ended;
 		rb = GetComponent<Rigidbody>();
-
-		keyboardAndMouse = true;
 	}
 
 	void Update() {
@@ -27,10 +25,18 @@ public class Player : MonoBehaviour{
 
 	private void FixedUpdate() {
 		if(canMove) {
-			Vector3 velocity = new Vector3(joystick.Horizontal, 0.0f, joystick.Vertical);
+			Vector3 velocity = new Vector3(joystick.Horizontal, 0, joystick.Vertical).normalized;
 			//rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime * speedMultiplier);
-			gameObject.transform.rotation = Quaternion.LookRotation(velocity).normalized;
-			gameObject.transform.Translate(velocity * speedMultiplier * Time.deltaTime, Space.World);
+			//rb.AddForce(velocity);
+			if(velocity.magnitude >= .1f) {
+				float targetAngle = Mathf.Atan2(velocity.x, velocity.z) * Mathf.Rad2Deg;
+				float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnTime);
+				transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+				gameObject.GetComponent<CharacterController>().Move(velocity * movementSpeed * Time.deltaTime);
+				
+			}
+			//gameObject.transform.rotation = Quaternion.LookRotation(velocity);
+			//gameObject.transform.Translate(velocity * movementSpeed * Time.deltaTime, Space.World);
 		}
 	}
 
@@ -39,55 +45,49 @@ public class Player : MonoBehaviour{
 	}
 
 	private void OnCollisionEnter(Collision collision) {
-		foreach(ContactPoint contact in collision.contacts) {
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
-			Debug.Log("Collision!"); //The collision of the player with objects and walls doesn't work...
-		}
+		Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.white);
+		Debug.Log("Collision!"); //The collision of the player with objects and walls doesn't work...
 	}
 
 	private void SelectedObject() {
 		bool isMorphed = gameObject.GetComponent<Morph>().isMorphed().Equals(false);
 		//For mouse
-		//if(keyboardAndMouse) {
-			if(Input.GetMouseButtonDown(0) && isMorphed) {
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 30f);
-				RaycastHit hit;
-				if(Physics.Raycast(ray, out hit)) {
-					//Debug.Log("Ray hit: " + hit.transform.name + ".");
-					if(hit.collider != null && hit.transform.tag.Equals("MorphableObject")) {
-						toggleMovement();
-						GameObject selectedObject = hit.transform.gameObject;
-						//Debug.Log("Selected: " + selectedObject + ".");
-						gameObject.GetComponent<Morph>().morphObject(selectedObject);
-					}
+		/*if(Input.GetMouseButtonDown(0) && isMorphed) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 30f);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				//Debug.Log("Ray hit: " + hit.transform.name + ".");
+				if(hit.collider != null && hit.transform.tag.Equals("MorphableObject")) {
+					toggleMovement();
+					GameObject selectedObject = hit.transform.gameObject;
+					//Debug.Log("Selected: " + selectedObject + ".");
+					gameObject.GetComponent<Morph>().morphObject(selectedObject);
 				}
-			} else if(Input.GetMouseButtonDown(0) && isMorphed) {
-				gameObject.GetComponent<Morph>().morphObject();
-				toggleMovement();
 			}
-		//}
+		} else if(Input.GetMouseButtonDown(0) && gameObject.GetComponent<Morph>().isMorphed().Equals(true)) {
+			gameObject.GetComponent<Morph>().morphObject();
+			toggleMovement();
+		}*/
 
 		//For touch, needs updating from For mouse part
 		//sauce: https://answers.unity.com/questions/1126621/best-way-to-detect-touch-on-a-gameobject.html
-		/*else if(!keyboardAndMouse) {
-			if(Input.touchCount > 0 && Input.GetTouch(0).phase.Equals(touchPhase) && isMorphed) {
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 30f);
-				RaycastHit hit;
-				if(Physics.Raycast(ray, out hit)) {
-					//Debug.Log("Ray hit: " + hit.transform.name + ".");
-					if(hit.collider != null && hit.transform.tag.Equals("MorphableObject")) {
-						toggleMovement();
-						GameObject selectedObject = hit.transform.gameObject;
-						//Debug.Log("Morphing into: " + selectedObject + ".");
-						gameObject.GetComponent<Morph>().morphObject(selectedObject);
-					}
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase.Equals(touchPhase) && gameObject.GetComponent<Morph>().isMorphed().Equals(false)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 30f);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				//Debug.Log("Ray hit: " + hit.transform.name + ".");
+				if(hit.collider != null && hit.transform.tag.Equals("MorphableObject")) {
+					toggleMovement();
+					GameObject selectedObject = hit.transform.gameObject;
+					//Debug.Log("Morphing into: " + selectedObject + ".");
+					gameObject.GetComponent<Morph>().morphObject(selectedObject);
 				}
-			} else if(Input.GetMouseButtonDown(0) && isMorphed) {
-				gameObject.GetComponent<Morph>().morphObject();
-				toggleMovement();
 			}
-		}*/
+		} else if(Input.GetMouseButtonDown(0) && gameObject.GetComponent<Morph>().isMorphed().Equals(true)) {
+			gameObject.GetComponent<Morph>().morphObject();
+			toggleMovement();
+		}
 	}
 }
