7f268b46c14f5a4aef33f99868810b0a048bf6ca
GUSTOfWinds <gustavkjonsson01@gmail.com>
2021-05-10 20:26:13 +0200

    InventorySystem - Player Feedback
    Implemented all the functionality for giving players feedback after picking up an item

--- Morph/Assets/Scripts/Item.cs
+++ Morph/Assets/Scripts/Item.cs
@@ -7,8 +7,6 @@ public class Item : MonoBehaviour
 	private void OnTriggerEnter(Collider collider) {
 		if(collider.gameObject.tag.Equals("Player")) {
 			collider.gameObject.GetComponent<ItemController>().addItem(gameObject);
-			Debug.Log("Player has picked up the " + gameObject.name + ".");
-
 			Destroy(gameObject);
 		}
 	}
--- Morph/Assets/Scripts/ItemController.cs
+++ Morph/Assets/Scripts/ItemController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour {
 
 	public List<GameObject> items = new List<GameObject>();
+
+	public GameObject textPopUp;
+	public ParticleSystem particlePopUp;
 
 	public GameObject getItem(GameObject getItem) {
 		return items.Contains(getItem) ? getItem : null;
@@ -20,5 +24,28 @@ public class ItemController : MonoBehaviour {
 
 	public void addItem(GameObject addItem) {
 		items.Add(addItem);
+		addPopup(addItem);
+		Debug.Log("Picked Up:" + addItem.name);
 	}
+
+    //
+    private void Start()
+    {
+		textPopUp.SetActive(false);
+
+	}
+	
+	//IMPLEMENT ANY SOUND TO BE PLAYED IN THIS METHOD BELOW
+	public void addPopup(GameObject item)
+    {
+		textPopUp.SetActive(true);
+		particlePopUp.Play();
+		Invoke("popupEnd", 1);
+
+    }
+	public void popupEnd()
+    {
+		textPopUp.SetActive(false);
+		particlePopUp.Stop();
+    }
 }
