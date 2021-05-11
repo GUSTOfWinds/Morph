44cc9242013037cfa8e43f930b9b48054dbb3ba0
GUSTOfWinds <gustavkjonsson01@gmail.com>
2021-05-11 14:11:55 +0200

    InventoryFeedback - Basic Structure
    Added the basic structure for the feedback system for the inventory system. This means that code can just be added to comment fields to enable various kinds of feedback to be provided to the player

--- Morph/Assets/Scripts/Item.cs
+++ Morph/Assets/Scripts/Item.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-	private void OnTriggerEnter(Collider collider) {
-		if(collider.gameObject.tag.Equals("Player")) {
+    private bool removeObject = false;
+    private bool canBeTaken = true;
+    private float timer = 0f;
+    private float deletionTime = 1f;
+
+    private void Update()
+    {
+        if (removeObject)
+        {
+            timer += Time.deltaTime;
+            if(timer >= deletionTime)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.tag.Equals("Player") && canBeTaken) {
+
 			collider.gameObject.GetComponent<ItemController>().addItem(gameObject);
-			Destroy(gameObject);
+            removeObject = true;
+            canBeTaken = false;
+
+            //Text Pop-Up
+            //Particle Effects
+            //Audio Effects
 		}
 	}
 }
--- Morph/Assets/Scripts/ItemController.cs
+++ Morph/Assets/Scripts/ItemController.cs
@@ -1,14 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ItemController : MonoBehaviour {
 
 	public List<GameObject> items = new List<GameObject>();
-
-	public GameObject textPopUp;
-	public ParticleSystem particlePopUp;
 
 	public GameObject getItem(GameObject getItem) {
 		return items.Contains(getItem) ? getItem : null;
@@ -24,28 +20,6 @@ public class ItemController : MonoBehaviour {
 
 	public void addItem(GameObject addItem) {
 		items.Add(addItem);
-		addPopup(addItem);
 		Debug.Log("Picked Up:" + addItem.name);
 	}
-
-    //
-    private void Start()
-    {
-		textPopUp.SetActive(false);
-
-	}
-	
-	//IMPLEMENT ANY SOUND TO BE PLAYED IN THIS METHOD BELOW
-	public void addPopup(GameObject item)
-    {
-		textPopUp.SetActive(true);
-		particlePopUp.Play();
-		Invoke("popupEnd", 1);
-
-    }
-	public void popupEnd()
-    {
-		textPopUp.SetActive(false);
-		particlePopUp.Stop();
-    }
 }
