bc3aa0a737487a8c2ff8b57423f2e9a881671ff2
GUSTOfWinds <gustavkjonsson01@gmail.com>
2021-05-13 17:03:48 +0200

    Changelog #6
    Implemented back all of the functionality, as well as juicing, that was missing after the merge

--- Morph/Assets/Scripts/PauseMenuManager.cs
+++ Morph/Assets/Scripts/PauseMenuManager.cs
@@ -9,14 +9,14 @@ public class PauseMenuManager : MonoBehaviour
     public static bool isGamePaused = false;
     public GameObject pauseMenu;
     public GameObject pauseButton;
-    public GameObject playerController;
+    public GameObject playerJoystickController;
 
     //This ensures the game is not paused when the scene begins
     private void Start()
     {
-        pauseMenu.SetActive(false);
-        pauseButton.SetActive(true);
-        isGamePaused = false;
+        PauseGame();
+        ResumeGame();
+
     }
 
     //These three methods are tied to buttons and fulfill all the functions of the menu, both actually pausing the game as well as enabling/disabling elements or changing scenes
@@ -24,7 +24,7 @@ public class PauseMenuManager : MonoBehaviour
     {
         pauseMenu.SetActive(false);
         pauseButton.SetActive(true);
-        playerController.SetActive(true);
+        playerJoystickController.SetActive(true);
         Time.timeScale = 1f;
         isGamePaused = false;
     }
@@ -32,13 +32,13 @@ public class PauseMenuManager : MonoBehaviour
     {
         pauseMenu.SetActive(true);
         pauseButton.SetActive(false);
-        playerController.SetActive(false);
+        playerJoystickController.SetActive(false);
         Time.timeScale = 0f;
         isGamePaused = true;
     }
     public void LoadMenu()
     {
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Main Menu");
         pauseMenu.SetActive(false);
         pauseButton.SetActive(false);
     }
