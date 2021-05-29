cb05e6bc561cec7c69e8d8c7c653c1712b1c927a
Markus <markus.larsson421@gmail.com>
2021-05-29 23:56:51 +0200

    Implemented sounds to door and laser but laser isn't work properly. Moved laser, keycard and card reader. Extended AudioManager with Stop method. Cleaned up a bit of code.

--- Morph/Assets/Scripts/Keypad.cs
+++ Morph/Assets/Scripts/Keypad.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Keypad : MonoBehaviour{
 	public GameObject player;
 	public GameObject keycard;
-	public GameObject activate;
+	public GameObject laser;
 	private void OnTriggerEnter(Collider collider) {
 		if(collider.transform.tag.Equals(player.tag) && player.GetComponent<ItemController>().hasItem(keycard)) {
-			activate.GetComponent<Laser>().turnOff();
+			laser.GetComponent<Laser>().turnOff();
 		}
 	}
 }
--- Morph/Assets/Scripts/Laser.cs
+++ Morph/Assets/Scripts/Laser.cs
@@ -5,33 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class Laser : MonoBehaviour{
 	private GameObject laserBeams;
+	private bool laserActive = true;
 
 	private void Start() {
 		laserBeams = gameObject.transform.GetChild(0).gameObject;
+		FindObjectOfType<AudioManager>().Play("Laser");
 	}
 
 	private void OnTriggerEnter(Collider collider) {
-		if(collider.tag.Equals("Player")) {
+		if(collider.tag.Equals("Player") && laserActive) {
 			SceneManager.LoadSceneAsync("Game Over"); //Använder Async eftersom Unity påstår att det är bättre. https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadSceneAsync.html
 		}
 	}
 
 	public bool turnOff() {
-		toggleLighting();
-		gameObject.SetActive(false);
+		foreach(Transform child in transform) {
+			child.gameObject.SetActive(false);
+		}
+		laserActive = false;
+		FindObjectOfType<AudioManager>().Stop("Laser");
 		return true;
 	}
 	public bool turnOn() {
-		toggleLighting();
-		gameObject.SetActive(true);
+		foreach(Transform child in transform) {
+			child.gameObject.SetActive(true);
+		}
+		laserActive = true;
+		FindObjectOfType<AudioManager>().Play("Laser");
 		return true;
 	}
 	public bool toggle() {
-		gameObject.SetActive(gameObject.activeSelf.Equals(true) ? false : true);
+		foreach(Transform child in transform) {
+			child.gameObject.SetActive(gameObject.activeSelf.Equals(true) ? false : true);
+		}
+		laserActive = laserActive ? false : true;
+		if(laserActive) {FindObjectOfType<AudioManager>().Play("Laser");} 
+		else if(!laserActive) {FindObjectOfType<AudioManager>().Stop("Laser");}
 		return true;
-	}
-
-	private void toggleLighting() {
-		
 	}
 }
